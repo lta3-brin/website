a12deb451fa6b7415c0adabfe3346a9543239d3a
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-04-22 16:07:16 +0700

    menerapkan vue props untuk komponen drawer

--- components/header_drawer.vue
+++ components/header_drawer.vue
@@ -1,8 +1,10 @@
 <template>
   <v-list>
-    <v-list-group v-for="item of items" :key="item.id">
+    <v-list-group v-for="item of dataHeader" :key="item.id">
       <template v-slot:activator>
-        <v-list-item-title>{{ item.nama }}</v-list-item-title>
+        <v-list-item-title>
+          {{ item.nama }}
+        </v-list-item-title>
       </template>
       <template v-slot:appendIcon>
         <v-icon v-if="item.subs.length > 0">fa-angle-down</v-icon>
@@ -22,8 +24,8 @@
           </v-list-item>
         </template>
         <template v-slot:prependIcon>
-          <v-icon v-if="sub.subs.length > 0">fa-caret-down</v-icon>
-          <div v-else></div>
+          <v-icon v-if="sub.subs.length > 0" size="18">fa-caret-down</v-icon>
+          <v-icon v-else size="12">fa-link</v-icon>
         </template>
 
         <v-list-item v-for="itm in sub.subs" :key="itm.id" link>
@@ -35,13 +37,21 @@
 </template>
 
 <script>
-import menuItem from '~/static/menu.json'
-
 export default {
   name: 'HeaderDrawer',
+  props: {
+    dataHeader: {
+      type: Array,
+      default: () => [],
+      required: true
+    }
+  },
   data() {
-    return {
-      items: menuItem
+    return {}
+  },
+  watch: {
+    selectHeader: (val, oldVal) => {
+      return val
     }
   }
 }
