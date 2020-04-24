3e6cda18ac0cb99be4e59aeb26ed5318695ca033
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-04-24 22:00:54 +0700

    membuat halaman index untuk keahlian

---
+++ pages/keahlian/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <Kosongan />
+</template>
+
+<script>
+import Kosongan from '~/components/empty'
+
+export default {
+  name: 'IndexKeahlian',
+  components: {
+    Kosongan
+  },
+  head() {
+    return {
+      title: 'Pemberitahuan',
+      meta: [
+        {
+          hid: 'index_keahlian',
+          name: 'description',
+          content: 'Pemberitahuan halaman keahlian utama'
+        }
+      ]
+    }
+  }
+}
+</script>
