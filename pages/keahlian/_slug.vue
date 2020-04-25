d6b20e17186dcc3b86afcfae6eeab2d0e6e19b51
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-04-25 12:32:20 +0700

    membuat halan baru untuk sub keahlian

---
+++ pages/keahlian/_slug.vue
@@ -0,0 +1,26 @@
+<template>
+  <Kosongan />
+</template>
+
+<script>
+import Kosongan from '~/components/empty'
+
+export default {
+  name: 'IndexKeahlian',
+  components: {
+    Kosongan
+  },
+  head() {
+    return {
+      title: `Informasi keahlian ${this.$route.params.slug}`,
+      meta: [
+        {
+          hid: 'slug_keahlian',
+          name: 'description',
+          content: `Halaman berkaitan informasi keahlian ${this.$route.params.slug}`
+        }
+      ]
+    }
+  }
+}
+</script>
