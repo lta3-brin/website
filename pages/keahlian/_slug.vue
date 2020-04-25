1f0dc936d19599fb0b20d8c7291404fb4b99644f
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-04-25 17:27:08 +0700

    melakukan penambahan komponen html renderer

--- pages/keahlian/_slug.vue
+++ pages/keahlian/_slug.vue
@@ -1,14 +1,47 @@
 <template>
-  <Kosongan />
+  <v-app dark>
+    <v-parallax dark height="500" :src="keahlian.rincian.thumbnail">
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h1 class="display-3 font-weight-thin mb-4 text-uppercase">
+            Keahlian {{ keahlian.nama }}
+          </h1>
+        </v-col>
+      </v-row>
+    </v-parallax>
+
+    <v-container class="fill-height">
+      <v-row align="center" justify="center">
+        <v-col cols="12">
+          <v-alert text border="top" colored-border color="red darken-4">
+            <h3 class="headline mb-7">Cakupan Kegiatan</h3>
+
+            <vue-markdown html :source="keahlian.rincian.deskripsi" />
+          </v-alert>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import Kosongan from '~/components/empty'
+import VueMarkdown from 'vue-markdown'
+import dataKeahlian from '~/static/keahlian.json'
 
 export default {
   name: 'IndexKeahlian',
   components: {
-    Kosongan
+    VueMarkdown
+  },
+  data() {
+    return {
+      keahlian: []
+    }
+  },
+  created() {
+    this.keahlian = dataKeahlian.subs.filter((keahlian) => {
+      return keahlian.slug === this.$route.params.slug
+    })[0]
   },
   head() {
     return {
