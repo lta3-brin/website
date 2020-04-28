5ce3b5d052c2b1fecc5eda1070d83a8f61d5e1fd
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-04-28 22:38:18 +0700

    membuat layout awal website untuksisi kanan menu

---
+++ layouts/right_sidebar.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app dark>
+    <Header />
+
+    <v-content>
+      <v-app dark>
+        <v-container>
+          <v-row align="start" justify="space-between">
+            <v-col cols="12" md="8">
+              <nuxt />
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-subheader>
+                SIDEBAR
+              </v-subheader>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-app>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Header from '~/components/header'
+// import Footer from '~/components/footer'
+
+export default {
+  components: {
+    Header
+    // Footer
+  },
+  data() {
+    return {}
+  }
+}
+</script>
