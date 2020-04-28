329877e3f48058e003b677ea00d6e1382de5de01
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-04-28 22:37:31 +0700

    membuat komponen baru sebagai tautan navigasi keahlian

---
+++ components/submenu/keahlian.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card tile>
+    <v-list shaped>
+      <v-subheader>KEAHLIAN</v-subheader>
+      <v-list-item-group color="primary">
+        <v-list-item v-for="ahli in keahlian.subs" :key="ahli.slug">
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ ahli.nama }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+import dataKeahlian from '~/static/keahlian.json'
+
+export default {
+  name: 'SubMenuKeahlian',
+  data() {
+    return {
+      keahlian: dataKeahlian
+    }
+  }
+}
+</script>
