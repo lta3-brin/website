facc440211df113092b8fd84674f5e0270d65f0f
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-04-28 22:35:59 +0700

    membuat komponen baru untuk tautan navigasi chat

---
+++ components/submenu/chat.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-btn
+    color="primary"
+    x-large
+    block
+    elevation="7"
+    target="_blank"
+    href="http://bit.ly/DiscordBBTA3"
+  >
+    MULAI OBROLAN
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'Chat'
+}
+</script>
