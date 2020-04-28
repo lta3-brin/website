990b9860e3a8679343e943aa1096f07c9715183c
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-04-28 22:37:51 +0700

    melakukan perubahan data meta website

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate:
-      '%s - ' + 'website resmi ' + process.env.npm_package_name + ' bppt',
+    titleTemplate: '%s - ' + 'Website Resmi BBTA3 BPPT',
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
