aa3b175de450213a81756e64ed54e31dd2793fda
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-04-29 20:36:12 +0700

    fetch twitter service dari bbta3 dan bppt

--- store/index.js
+++ store/index.js
@@ -4,29 +4,36 @@ export const actions = {
   async nuxtServerInit({ commit, dispatch }) {
     const berita = await dispatch('fetchTwitetrTimeline')
 
-    berita.forEach((brt) => {
-      commit('berita/addBerita', {
-        id: brt.id,
-        createdAt: brt.created_at,
-        description: brt.full_text,
-        thumbnail: brt.entities.media[0].media_url,
-        screen_name: brt.user.screen_name
-      })
-    })
+    commit('berita/addBerita', berita.bbta3)
+    commit('berita/addBerita', berita.bppt)
   },
   async fetchTwitetrTimeline() {
     const key = process.env.NEWS_API
-    const URL = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=bbta3_bppt&count=4&exclude_replies=true&tweet_mode=extended`
+    const URL_BBTA3 = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=bbta3_bppt&count=4&exclude_replies=true&tweet_mode=extended`
+    const URL_BPPT = `https://api.twitter.com/1.1/search/tweets.json?q=from:bppt_ri&result_type=recent&count=3&tweet_mode=extended`
+
     try {
-      const res = await axios.get(URL, {
+      const resBBTA3 = await axios.get(URL_BBTA3, {
         headers: {
           Authorization: `Bearer ${key}`
         }
       })
 
-      return res.data
+      const resBPPT = await axios.get(URL_BPPT, {
+        headers: {
+          Authorization: `Bearer ${key}`
+        }
+      })
+
+      return {
+        bbta3: resBBTA3.data,
+        bppt: resBPPT.data.statuses
+      }
     } catch (_) {
-      return []
+      return {
+        bbta3: [],
+        bppt: []
+      }
     }
   }
 }
