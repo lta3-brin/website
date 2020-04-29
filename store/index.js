ce0f656a10f91943ff1fe337fec2d2d7d365f825
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-04-29 22:14:08 +0700

    menerapkan dispatch dan commit untuk data video

--- store/index.js
+++ store/index.js
@@ -1,39 +1,10 @@
-import axios from 'axios'
-
 export const actions = {
   async nuxtServerInit({ commit, dispatch }) {
-    const berita = await dispatch('fetchTwitetrTimeline')
+    const berita = await dispatch('berita/fetchTwitter')
+    const video = await dispatch('video/fetchYoutube')
 
     commit('berita/addBerita', berita.bbta3)
     commit('berita/addBerita', berita.bppt)
-  },
-  async fetchTwitetrTimeline() {
-    const key = process.env.NEWS_API
-    const URL_BBTA3 = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=bbta3_bppt&count=4&exclude_replies=true&tweet_mode=extended`
-    const URL_BPPT = `https://api.twitter.com/1.1/search/tweets.json?q=from:bppt_ri&result_type=recent&count=3&tweet_mode=extended`
-
-    try {
-      const resBBTA3 = await axios.get(URL_BBTA3, {
-        headers: {
-          Authorization: `Bearer ${key}`
-        }
-      })
-
-      const resBPPT = await axios.get(URL_BPPT, {
-        headers: {
-          Authorization: `Bearer ${key}`
-        }
-      })
-
-      return {
-        bbta3: resBBTA3.data,
-        bppt: resBPPT.data.statuses
-      }
-    } catch (_) {
-      return {
-        bbta3: [],
-        bppt: []
-      }
-    }
+    commit('video/addVideo', video)
   }
 }
