1fd333a6500200207656ab642d1345444325d158
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-04-29 22:12:48 +0700

    memasang action state kedalam state module berita

--- store/berita.js
+++ store/berita.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 export const state = () => ({
   koleksi: []
 })
@@ -13,5 +15,37 @@ export const mutations = {
         screen_name: brt.user.screen_name
       })
     })
+  }
+}
+
+export const actions = {
+  async fetchTwitter() {
+    const key = process.env.NEWS_API
+    const URL_BBTA3 = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=bbta3_bppt&count=4&exclude_replies=true&tweet_mode=extended`
+    const URL_BPPT = `https://api.twitter.com/1.1/search/tweets.json?q=from:bppt_ri&result_type=recent&count=3&tweet_mode=extended`
+
+    try {
+      const resBBTA3 = await axios.get(URL_BBTA3, {
+        headers: {
+          Authorization: `Bearer ${key}`
+        }
+      })
+
+      const resBPPT = await axios.get(URL_BPPT, {
+        headers: {
+          Authorization: `Bearer ${key}`
+        }
+      })
+
+      return {
+        bbta3: resBBTA3.data,
+        bppt: resBPPT.data.statuses
+      }
+    } catch (_) {
+      return {
+        bbta3: [],
+        bppt: []
+      }
+    }
   }
 }
