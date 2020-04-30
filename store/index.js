26f608f15e7eb735f2a6ef752dccde3e4a98b3e3
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-04-30 07:55:39 +0700

    melakukan dispatch dan commit untuk playlist video service di sisi server

--- store/index.js
+++ store/index.js
@@ -2,9 +2,11 @@ export const actions = {
   async nuxtServerInit({ commit, dispatch }) {
     const berita = await dispatch('berita/fetchTwitter')
     const video = await dispatch('video/fetchYoutube')
+    const playlist = await dispatch('video/fetchYoutubePlaylist')
 
     commit('berita/addBerita', berita.bbta3)
     commit('berita/addBerita', berita.bppt)
     commit('video/addVideo', video)
+    commit('video/addPlaylist', playlist)
   }
 }
