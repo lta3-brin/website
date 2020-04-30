abff5519e0bb8d0e00594a846ab0ab2db1b76f1c
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-04-30 07:57:01 +0700

    menambahkan state, mutation dan actions untuk kelola playlist video

--- store/video.js
+++ store/video.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export const state = () => ({
-  koleksi: []
+  koleksi: [],
+  playlist: []
 })
 
 export const mutations = {
@@ -15,6 +16,14 @@ export const mutations = {
         title: video.snippet.title
       })
     })
+  },
+  addPlaylist(state, payload) {
+    payload.forEach((playlist) => {
+      state.playlist.push({
+        id: playlist.id,
+        title: playlist.snippet.title
+      })
+    })
   }
 }
 
@@ -23,6 +32,17 @@ export const actions = {
     try {
       const key = process.env.VIDEO_API
       const URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCa0_hm_SiHxps1Llk_q6I1Q&order=date&type=video&maxResults=10&key=${key}`
+      const RES = await axios.get(URL)
+
+      return RES.data.items
+    } catch (_) {
+      return []
+    }
+  },
+  async fetchYoutubePlaylist() {
+    try {
+      const key = process.env.VIDEO_API
+      const URL = `https://www.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&maxResults=25&channelId=UCa0_hm_SiHxps1Llk_q6I1Q&key=${key}`
       const RES = await axios.get(URL)
 
       return RES.data.items
