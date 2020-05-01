cd7662aa80bfda0ed3e747629f50e86866c37d30
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-05-01 14:46:39 +0700

    membuat halaman untuk data tentang slug

---
+++ pages/tentang/_slug.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-subheader> INI HALAMAN TENTANG {{ slug }} </v-subheader>
+</template>
+
+<script>
+export default {
+  name: 'SlugTentang',
+  layout: 'right_sidebar',
+  computed: {
+    slug() {
+      return this.$route.params.slug
+    }
+  }
+}
+</script>
