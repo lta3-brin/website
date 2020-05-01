28341758bc8fa4cad25bf27d20e7b952470fce05
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-05-01 17:22:12 +0700

    kelola data bagian tentang fasilitas

--- pages/tentang/_slug.vue
+++ pages/tentang/_slug.vue
@@ -1,15 +1,33 @@
 <template>
-  <v-subheader> INI HALAMAN TENTANG {{ slug }} </v-subheader>
+  <article class="artikel">
+    <vue-markdown :source="tentang.rincian.deskripsi" />
+  </article>
 </template>
 
 <script>
+import VueMarkdown from 'vue-markdown'
+import Tentang from '~/static/tentang.json'
+
 export default {
   name: 'SlugTentang',
   layout: 'right_sidebar',
+  components: {
+    VueMarkdown
+  },
+  data() {
+    return {
+      tentang: null
+    }
+  },
   computed: {
     slug() {
       return this.$route.params.slug
     }
+  },
+  created() {
+    this.tentang = Tentang.subs.filter((sub) => {
+      return sub.slug === this.slug
+    })[0]
   }
 }
 </script>
