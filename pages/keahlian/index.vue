760ed5c145ea793df9b00b863b4c0414a6d2b500
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-05-01 14:12:13 +0700

    penyederhanaan halaman index masing-masing router

--- pages/keahlian/index.vue
+++ pages/keahlian/index.vue
@@ -1,26 +1,8 @@
-<template>
-  <Kosongan />
-</template>
-
 <script>
-import Kosongan from '~/components/empty'
-
 export default {
   name: 'IndexKeahlian',
-  components: {
-    Kosongan
-  },
-  head() {
-    return {
-      title: 'Pemberitahuan',
-      meta: [
-        {
-          hid: 'index_keahlian',
-          name: 'description',
-          content: 'Pemberitahuan halaman keahlian utama'
-        }
-      ]
-    }
+  middleware({ redirect }) {
+    return redirect('/')
   }
 }
 </script>
