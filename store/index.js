a8e7d4eab670677f5315db6bb41a307b1052f4f1
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-05-18 16:59:08 +0700

    melakukan penyesuaian dispatch state untuk video dan playlist

--- store/index.js
+++ store/index.js
@@ -1,13 +1,11 @@
 export const actions = {
   async nuxtServerInit({ commit, dispatch }) {
     const berita = await dispatch('berita/fetchTwitter')
-    const video = await dispatch('video/fetchYoutube')
-    const playlist = await dispatch('video/fetchYoutubePlaylist')
+    await dispatch('video/readVideo')
+    await dispatch('video/readPlaylist')
 
     commit('berita/addHashtag', berita.bbta3)
     commit('berita/addBerita', berita.bbta3)
     commit('berita/addBerita', berita.bppt)
-    commit('video/addVideo', video)
-    commit('video/addPlaylist', playlist)
   }
 }
