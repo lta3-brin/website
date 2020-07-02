8396e94a107ee7be83026685fe110b4dc65e2fcc
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-07-02 11:52:39 +0700

    membuat animasi ketik teks

---
+++ themes/bbta3/static/js/typeanim.js
@@ -0,0 +1,10 @@
+const options = {
+  stringsElement: 'div#keahlian',
+  startDelay: 1000,
+  typeSpeed: 80,
+  backSpeed: 60,
+  loop: true,
+  smartBackspace: false
+};
+
+const typed = new Typed('span#anim', options);
